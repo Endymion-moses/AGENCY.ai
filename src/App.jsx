edf2f920c84c718410af55e244e0b69e1728ba87
Endymion Moses <silemmoses@gmail.com>
2026-04-14 11:18:41 +0300

    feat:add implement responsive Navbar with sidebar toggle,theme switching and connect button feat; add 'Trusted by 10k+ people' button with profile visuals

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero.jsx'
+
+const App = () => {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') ?
+    localStorage.getItem('theme') : "light")
+  return (
+    <div className='dark:bg-black relative'>
+       < Navbar theme = {theme} setTheme={setTheme}/>
+       <Hero/>
+    </div>
+  )
+}
+
+export default App
